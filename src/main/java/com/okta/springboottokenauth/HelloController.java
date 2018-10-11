3b1d1732ba88ef0c4de33ebcafda8d093603fb69
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-10-11 10:20:46 -0700

    using @RestController

--- src/main/java/com/okta/springboottokenauth/HelloController.java
+++ src/main/java/com/okta/springboottokenauth/HelloController.java
@@ -1,19 +1,13 @@
 package com.okta.springboottokenauth;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
-@Controller
+@RestController
 @RequestMapping("api")
 public class HelloController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String home(java.security.Principal user) {
         if (user != null) {
             return "Hello " + user.getName();
